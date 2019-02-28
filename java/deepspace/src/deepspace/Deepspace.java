1605078542ad254e4139fc22bac9d9652a58021a
danikc <danielkc@correo.ugr.es>
2019-02-28 13:13:14 +0100

    P1/Java: Acabo la clase Dice
    Buen dia igualmente Blanquilla

--- java/deepspace/src/deepspace/Deepspace.java
+++ java/deepspace/src/deepspace/Deepspace.java
@@ -158,22 +158,50 @@ class Dice {
     }
     
     public int initWithNHangars() {
-        float p = new Random().nextFloat();
-        if(p <= NHANGARSPROB) {
+        float p = generator.nextFloat();
+        if(p < NHANGARSPROB) {
             return 0;
         }
         return 1;
     }
     
     public int initWithNWeapons() {
-        float p = new Random().nextFloat();
+        float p = generator.nextFloat();
         if(p < NWEAPONSPROB) {
             return 1;
         }
-        if(p <= 2*NWEAPONSPROB) {
+        if(p < 2*NWEAPONSPROB) {
             return 2;
         }
         return 3;
+    }
+    
+    public int initWithNShields() {
+        float p = generator.nextFloat();
+        if(p<NSHIELDSPROB) {
+            return 0;
+        }
+        return 1;
+    }
+    
+    public int whoStarts(int nPlayers) {
+        return generator.nextInt(nPlayers);
+    }
+    
+    GameCharacter firstShot() {
+        float p = generator.nextFloat();
+        if(p<FIRSTSHOTPROB) {
+            return  GameCharacter.SPACESTATION;
+        }
+        return GameCharacter.ENEMYSTARSHIP;
+    }
+    
+    boolean spaceStationMoves(float speed) {
+        float p = generator.nextFloat();
+        if(p<speed) {
+            return true;
+        }
+        return false;
     }
 }
 /**
